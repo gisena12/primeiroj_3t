17512daaca85cb2331714ea91730b9a15ba0f6ae
gisena12 <106184169+gisena12@users.noreply.github.com>
2022-10-04 15:41:18 -0300

    04-10

--- estudo.js
+++ estudo.js
@@ -3,14 +3,36 @@
 let tabuada = 4
 function meChame (){
 document.write ("tabuada do " + tabuada + "<br>")
-document.write ("tabuada + "x  1 = (tabuada*1)"<br>")
-document.write ("tabuada + "x  2 = (tabuada*2)"<br>")
-document.write ("tabuada + "x 3 = (tabuada*3) "<br>")
-document.write ("tabuada + "x  4 = (tabuada*4) "<br>")
-document.write ("tabuada + "x  5 = (tabuada*5) "<br>")
-document.write ("tabuada + "x 6 = (tabuada*6)"<br>")
-document.write ("tabuada + "x 7 = (tabuada*7)"<br>")
-document.write ("tabuada + "x 8 = (tabuada*8)"<br>")
-document.write ("tabuada + "x 9 = (tabuada*9)"<br>")
-document.write ("tabuada + "x 10 = (tabuada*10) "<br>")
+document.write (tabuada + "x  1 = "(tabuada*1)+ "<br>")
+document.write (tabuada + "x  2 = "(tabuada*2)+ "<br>")
+document.write (tabuada + "x 3 = "(tabuada*3) + "<br>")
+document.write (tabuada + "x  4 = "(tabuada*4)+ "<br>")
+document.write (tabuada + "x  5 ="(tabuada*5)+ "<br>")
+document.write (tabuada + "x 6 ="(tabuada*6)+"<br>")
+document.write (tabuada + "x 7 ="(tabuada*7)+"<br>")
+document.write (tabuada + "x 8 =" (tabuada*8)+"<br>");
+document.write (tabuada + "x 9 =" (tabuada*9)+"<br>")
+document.write (tabuada + "x 10 = "(tabuada*10) +"<br>")
+
+}
+
+function minhaTabuada () {
+for(var i=1; i<10;i++) {
+   
+   for(var j= 1; j<=10; j++){
+
+   
+    document.write (tabuada + " x " +i+"  =" + (i*j)(tabuada*i)+"<br>")
+   }
+document.write( "<br><br>")
+}
+ 
+}
+
+function quadrado () {
+ for(var i= 2; i<21; i++){
+    document.write ("O quadrado de" +i+ " é " + (i*i)) + "<br>")
+
+ }
+
 }
